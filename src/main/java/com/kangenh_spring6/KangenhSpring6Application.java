3557bc52187d24b712200c5c5325510a6c494a2f
yuda3 <yuda3@hotmail.com>
2024-07-05 14:21:42 +0900

    project start spring6

---
+++ src/main/java/com/kangenh_spring6/KangenhSpring6Application.java
@@ -0,0 +1,13 @@
+package com.kangenh_spring6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KangenhSpring6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KangenhSpring6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kangenh_spring6/KangenhSpring6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kangenh_spring6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KangenhSpring6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
